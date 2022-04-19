d1e53d6681428910deed11d5bb0f771b4dbcf438
TommyBoyUW <sepant@uw.edu>
2022-04-18 23:45:48 -0400

    refined and reorganized, need to clean up

--- lesson03/assignment/index.js
+++ lesson03/assignment/index.js
@@ -1,99 +1,62 @@
+
 //Assignment 03 - Constructors and Data
 
+//fetch JSON data from file
+fetch('./data/data.json')
+          .then(function (response) {
+              return response.json();
+          })
+          //address JSON data here
+          .then(function (data) {
+              appendData(data);
 
-//fetch('./data/data.json')
-//  .then((response) => {
-  //  return response.json()
-//  })
-//  .then((data) => {
-    // Work with JSON data here
+          })
+          //catch error if fail to load
+          .catch(function (err) {
+              console.log('error: ' + err);
+          });
 
-//    console.log(data)
-//let x = data;
-//console.log(x)
-//})
-//  .catch((err) => {
-    // Do something for an error here
-//  })
-
-
-
-const carsJSON = `[
-  {
-    "name": "PPaolo Maldini",
-    "jobTitle": "Front End Developer",
-    "company": "Web Weavers",
-    "experience": "3 years",
-    "school": "University of Washington",
-    "major": "Marketing",
-    "email": "paolo@example.com",
-    "linkedInUrl": "paolo.linkedinprofile.com",
-    "codeLanguages": [
-        "HTML", "CSS", "JavaScript", "Node", "Express"
-    ]
-  },
-  {
-    "name": "Barbara Bonansea",
-    "jobTitle": "Software Engineer",
-    "company": "Excellence in the Cloud",
-    "experience": "12 years",
-    "school": "University of Southern California",
-    "major": "Computer Science",
-    "email": "barbara@example.com",
-    "linkedInUrl": "barbara.linkedinprofile.com",
-    "codeLanguages": [
-        "HTML", "JavaScript", "C", "Go"
-    ]
-  },
-  {
-    "name": "Javier Hernandez",
-    "jobTitle": "User Experience Engineer",
-    "company": "Web Sites and More",
-    "experience": "5 years",
-    "school": "Seattle University",
-    "major": "Performing Arts",
-    "email": "javier@example.com",
-    "linkedInUrl": "javier.linkedinprofile.com",
-    "codeLanguages": [
-        "HTML", "CSS"
-    ]
-  },
-  {
-    "name": "Maribel Dominguez",
-    "jobTitle": "Front End Engineer",
-    "company": "Bits and Bytes",
-    "experience": "6 years",
-    "school": "University of Washington",
-    "major": "Mechanical Engineering",
-    "email": "maribel@example.com",
-    "linkedInUrl": "maribel.linkedinprofile.com",
-    "codeLanguages": [
-       "HTML", "CSS", "JavaScript", "React", "Vue", "Redux"
-    ]
-  }
-]`
-
-const cars = JSON.parse(carsJSON)
-console.log(Object.entries(cars));
-console.log(Object.values(cars));
+function appendData(data) {
+const cards = data;
+const container = document.querySelector('#template-hook')
+let cardList = cards.forEach(function(el){
+  // Loop through all elements of the list
 
 
-
-const container = document.querySelector('#list-container')
-
- //Loop through all elements of the list
-let carsHTML = cars.map(function(el) {
-let car =`<article class="car">
-   <ul>
-    <li class="left1">Name: ${el.name}</li>
-    <li class="right">School: ${el.school}</li>
-    <li class="right">Major: ${el.major}</li>
-    <li class="right">Email: ${el.email}</li>
-   </ul>
-  </article>`;
-
-console.log(car);
-
-return car;
-
-})
+    // Build HTML code
+    let list = document.createElement('div')
+    list.innerHTML = `
+    <div class="profile">
+      <div class="left">
+        <div class="left1">
+      <img src="img/headshot.jpg"  class="photo" alt="Paolo is ">
+    </div>
+    <div class="left1">
+      <p class="name">${el.name}</p>
+    </div>
+    <div class="left1">
+      <p class="jobtitle">${el.jobTitle}</p>
+    </div>
+    </div>
+      <div class="right">
+        <p><strong>Company: </strong>${el.company}</p>
+        <p><strong>Experience: </strong>${el.experience}</p>
+        <p><strong>School: </strong>${el.school}</p>
+        <p><strong>Major: </strong>${el.major}</p>
+        <p><strong>Email: </strong>${el.email}</p><br>
+        <p><strong>Languages: </strong>${el.codeLanguages}</p><br>
+        <div class="linkedin">
+          <div class="linkedin">
+        <img src="img/linkedin.svg"  class="logo" alt="linkedin">
+      </div>
+      <div class="linkedin" id="email">
+        <p id="email">${el.linkedInUrl}</p>
+      </div>
+    </div>
+    </div>
+  </div>
+    `
+    // Insert HTML into page
+    container.append(list)
+  })
+};
